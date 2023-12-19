cc8f397e4a49c8dd300a76f3e58d52dfa698f9e4
Nicholas Lima <nicholasveloso@gmail.com>
2023-12-19 17:24:02 +0000

    "Mudança no prompt da tool."

--- tools/degree.py
+++ tools/degree.py
@@ -52,7 +52,7 @@ RETURN
       END
   ) AS pathBetweenPeople
 ```
-
+Note: Tell the number of degrees of separation at the beggining of your answer.
 Note: Do not include any explanations or apologies in your responses.
 Do not respond to any questions that might ask anything else than for you to construct a Cypher statement.
 Do not include any text except the generated Cypher statement.
