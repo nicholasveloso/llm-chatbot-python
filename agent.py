9d76922ee13d5f8ebc69ea0182af605c5467a8db
Nicholas Lima <nicholasveloso@gmail.com>
2023-12-19 17:15:38 +0000

    "Adicionando nova ferramenta."

--- agent.py
+++ agent.py
@@ -11,19 +11,26 @@ from llm import llm
 
 # Usando a cadeias definida como ferramenta para o agente em vector.py.
 from tools.vector import kg_qa
-# Usando a cadeias definida como ferramenta para o agente em vector.py.
+# Usando a cadeias definida como ferramenta para o agente em cypher.py.
 from tools.cypher import cypher_qa
+# Usando a cadeias definida como ferramenta para o agente em degree.py.
+from tools.degree import cypher_degree
 
 # Definindo as ferramentas.
 tools = [
+    # Ferramenta para fazer a buscas do número de graus de separação e caminho entre duas pessoas.
+        Tool.from_function(
+        name="Cypher Six Degrees of Kevin Bacon",
+        description="Provide information about movie questions on how to connect an actor, actress, or director, and then connect them to another actor, actress, or director via a film that both actors have appeared in together, repeating this process to try to find the shortest path that ultimately leads to prolific American actor Kevin Bacon.",
+        func = cypher_degree,
+    ),
     # Ferramenta para fazer busca na nase de dados de grafos usando Cypher.
         Tool.from_function(
         name="Cypher QA",
-        description="Provide information about movies questions using Cypher. Also allows to find the path between two pepople and their degree separation.",
+        description="Provide information about movies questions using Cypher. Also, use to find how many degrees of separation are there, and the path, between two people.",
         func = cypher_qa,
     ),
-    # Ferramenta para fazer busca semântica usando o Vector 
-    # Index e retornar informações sobre enredo dos filmes.
+    # Ferramenta para fazer busca semântica usando o Vector Index e retornar informações sobre enredo dos filmes.
     Tool.from_function(
         name="Vector Search Index",  # (1)
         description="Provides information about movie plots using Vector Search", # (2)
@@ -60,8 +67,8 @@ agent = initialize_agent(
 # Criando a função para gerar resposta.
 def generate_response(prompt):
     """
-    Create a handler that calls the Conversational agent
-    and returns a response to be rendered in the UI
+    Cria um handler que chama o agente conversacional 
+    e retorna a resposta a ser renderizada na UI
     """
 
     response = agent(prompt)
